d6f3c38d4135670e47786aabac1d03c1f48729dd
Tanachi <tanachige@gmail.com>
2016-06-29 18:49:30 -1000

    Clients are able to leave now

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var net = require('net');
+var a = process.stdin;
+a.setEncoding('utf-8');
 var b = process.stdout;
 var CONFIG = require('./config');
 
@@ -10,15 +12,25 @@ var server = net.createServer(function(socket){
     connections.push(socket);
     socket.write('Connected to ' + CONFIG.PORT + '\n', 'utf8');
     socket.on('data', function(chunk){
-      b.write('Server Bcast from'  + socket.remoteAddress + ': ' + socket.remotePort +  ': ' +chunk,'utf8');
+      b.write('Server Bcast from'  + socket.remoteAddress + ': ' + socket.remotePort +  ': ' + chunk + '\n','utf8');
       for(var i = 0; i < connections.length; i++){
-        connections[i].write( connections[i].remoteAddress + ': ' +connections[i].remotePort + ': '  + chunk,'utf8');
+        connections[i].write( connections[i].remoteAddress + ': ' +connections[i].remotePort + ': '  + chunk + '\n','utf8');
       }
     });
     count++;
 
     socket.on('close', function(){
-      console.log('goodbye ]n');
+      var closedPort;
+      for(var z = 0; z < connections.length; z++){
+        if(connections[z].remotePort === socket.remotePort){
+          closedPort = connections[z].remotePort;
+          b.write('Closed' + connections[z].remotePort + '\n');
+          connections.splice(z, 1);
+        }
+      }
+      for(var x = 0; x < connections.length;x++){
+         connections[x].write( closedPort + ' left \n','utf8');
+      }
     });
 });
 
