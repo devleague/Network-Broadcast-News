0d00377f365c81b790c7c78f3e178c655a8d0fe5
Tanachi <tanachige@gmail.com>
2016-06-29 18:36:37 -1000

    fixed server messages

--- server.js
+++ server.js
@@ -6,16 +6,20 @@ var PORT = 6969;
 var count = 0;
 var connections = [];
 var server = net.createServer(function(socket){
-    console.log('someone connected \n');
+    b.write( socket.remoteAddress + ': ' + socket.remotePort + ' connected \n');
     connections.push(socket);
     socket.write('Connected to ' + CONFIG.PORT + '\n', 'utf8');
     socket.on('data', function(chunk){
-      b.write('Server Bcast from'  +socket.remoteAddress + ': ' + socket.remotePort +  ': ' +chunk,'utf8');
+      b.write('Server Bcast from'  + socket.remoteAddress + ': ' + socket.remotePort +  ': ' +chunk,'utf8');
       for(var i = 0; i < connections.length; i++){
         connections[i].write( connections[i].remoteAddress + ': ' +connections[i].remotePort + ': '  + chunk,'utf8');
       }
     });
     count++;
+
+    socket.on('close', function(){
+      console.log('goodbye ]n');
+    });
 });
 
 
