1bae171b9d9555fb60b8d730cccffe310b43ec49
Tanachi <tanachige@gmail.com>
2016-06-29 19:36:57 -1000

    Added username function

--- server.js
+++ server.js
@@ -5,20 +5,30 @@ var b = process.stdout;
 var CONFIG = require('./config');
 
 var PORT = 6969;
-var count = 0;
 var connections = [];
+var userCount = 0;
 var server = net.createServer(function(socket){
     b.write( socket.remoteAddress + ': ' + socket.remotePort + ' connected \n');
-    connections.push({socket:socket, userName:null});
+    connections.push({socket:socket, userName:''});
     socket.write('Connected to ' + CONFIG.PORT + '\n', 'utf8');
+    socket.write('Please enter username \n', 'utf8');
     socket.on('data', function(chunk){
+      if(connections.length > userCount){
+        connections[userCount].userName = chunk;
+        userCount++;
+      }
+      else{
+        var senderName;
+        for(var y = 0; y < connections.length; y++){
+          if(connections[y].socket.remotePort === socket.remotePort)
+            senderName = connections[y].userName.toString().replace(/(\r\n|\n|\r)/gm,"");
+        }
       b.write('Server Bcast from'  + socket.remoteAddress + ': ' + socket.remotePort +  ': ' + chunk + '\n','utf8');
-      for(var i = 0; i < connections.length; i++){
-        connections[i].socket.write( connections[i].socket.remoteAddress + ': ' +
-        connections[i].socket.remotePort + ': '  + chunk + '\n','utf8');
+        for(var i = 0; i < connections.length; i++){
+          connections[i].socket.write( senderName + ': '  + chunk + '\n','utf8');
+        }
       }
     });
-    count++;
 
     socket.on('close', function(){
       var closedPort;
@@ -27,6 +37,7 @@ var server = net.createServer(function(socket){
           closedPort = connections[z].socket.remotePort;
           b.write('Closed ' + connections[z].socket.remotePort + '\n');
           connections.splice(z, 1);
+
         }
       }
       for(var x = 0; x < connections.length;x++){
