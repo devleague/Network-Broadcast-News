fb72649a66d7e2c082e9eabda2c7732b19197c30
marunoa <marunoafp@gmail.com>
2016-06-30 19:10:06 -1000

    got clients and admin name displayed. Working on excluding already used names and admin name now

--- server.js
+++ server.js
@@ -1,36 +1,39 @@
 var net = require('net');
-var fs = require('fs');
 var CONFIG = require('./config');
 var user = [];
 
 var server = net.createServer (function (socket) {
 
-  var remoteAddress = socket.remoteAddress + ': ' + socket.remotePort;
+  var remoteAddress = socket.remoteAddress + ' PORT: ' + socket.remotePort;
 
   console.log('Somebody connected!');
-  console.log('connection data from: ' + remoteAddress);
+  console.log('Connection data from: ' + remoteAddress);
 
   // Connection Listener
+
+  server.on('error', function (error) {
+  if (error.code === 'EADDRINUSE') {
+    console.log('Adress already in use. Trying to reconnect...');
+    setTimeout (function () {
+    server.close();
+    server.listen(PORT);
+    }, 1000);
+  }
+});
 
   socket.setEncoding('utf8');
 
   user.push(socket);
 
   socket.on('data', function (data) {
-
     process.stdout.write(data);
-
     user.forEach(function (person) {
       person.write(data);
-
     });
-
   });
 
   socket.on('end', function () {
-
-    console.log(remoteAddress + ' disconnected.');
-
+    console.log(remoteAddress + ' just disconnected.');
   });
 
   socket.on('error', function (error) {
@@ -39,25 +42,18 @@ var server = net.createServer (function (socket) {
 
 });
 
+process.stdin.on('data', function (data) {
+  user.forEach(function(person) {
+    person.write('[ADMIN]: ' + data);
+  });
+});
+
 server.listen(CONFIG.PORT, function () {
   var PORT = server.address().port;
-  console.log('Listening on port ', PORT);
-
+  console.log('Listening on port:', PORT);
 });
 
-server.on('error', function (error) {
-
-  if (error.code === 'EADDRINUSE') {
-
-    console.log('Adress already in use. Trying to reconnect...');
-
-    setTimeout (function () {
-      server.close();
-      server.listen(PORT);
-    }, 1000);
-
-  }
-
+server.on('close', function () {
+  server.close();
 });
-
 
